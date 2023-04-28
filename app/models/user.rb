6670ff06a2c2599da57dbf02007dfdca30aaa3de
tkohei57 <con.kt.57@gmail.com>
2023-04-28 17:49:26 +0900

    messagesテーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :messages
   has_many :room_users
   has_many :rooms, through: :room_users
       
