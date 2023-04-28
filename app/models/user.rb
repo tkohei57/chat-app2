594c77463f34e8d60193af3ff405e268bbc4196c
tkohei57 <con.kt.57@gmail.com>
2023-04-28 16:52:39 +0900

    プルダウンの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :room_users
-  has_many :rooms, thorugh :room_users
+  has_many :rooms, through: :room_users
       
   validates :name, presence: true
 end
